78e6255e77a547cd883cd7cd97b00e914e08c870
Fariha Rahman <93405460+Rahmanfariha01@users.noreply.github.com>
2021-11-23 20:04:10 +0600

    Add files via upload

---
+++ basic of python.py
@@ -0,0 +1,83 @@
+# this is a method of print any kind of sentence 
+
+#print("Hello World")
+ 
+#text = "Hello World"
+#print(text)
+
+#this is a method of addition
+
+#number1 = 30 
+#number2 = 387
+#result = (number2 + number2)
+#print(result)
+
+#number1 = 10 
+#print(number1)
+#number1 = 15 
+#number1 = 18 
+#print(number1)
+
+#the process of showing " data type"
+#v = 10 
+#print(type(v))
+#v = "10"
+#print(type(v))
+#v= 10.0 
+#print(type(v))
+
+# there are some example of string 
+#s = "abcdefghigkl6788$##!"
+#print(s
+#s = ''
+#print(s)
+#s = ""
+#print(s)
+
+# convert to "data type" 
+#a = 1 
+#b = 2.3 
+#c = a+b 
+#print(c)
+
+# how can I devide number 
+#print(5/2)
+
+# how can I subtraction number 
+#print(5-2)
+
+# how can I Multiplication number 
+#print(5*2)
+
+#how can I remainder number 
+#print(5//2)
+
+#how can I modulus number 
+#print(5%2)
+
+#how can I square number 
+#print(5**2)
+
+#there is a some example of argument 
+#a =1 
+#b = 1.2 
+#c = "hello"
+#print(a, b, c)
+#print(a, b, c, "world")
+
+# there are some example of "input function"
+
+#name = input("What's your name ?")
+#print("Welcome to Python,",name, "!")
+
+#number1 = input("Please type an integer and press enter:")
+#number2 = input("Please type another integer snd press enter: ")
+#print("number1 + number2 =", number1+number2)
+
+#number1 = input("Please type an integer and press enter:")
+#number2 = input("plese type another integer and press enter:")
+#number1 = int(number1)
+#number2 = int(number2)
+#print("number1 + number2=", number1+number2)
+
+
